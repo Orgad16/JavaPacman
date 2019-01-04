f6a11e60c3c37c9e43301c8d0d64eaefe94091fb
Unknown <goldfedertomer@gmail.com>
2019-01-04 17:23:46 +0200

    added animation to map selection

--- JavaPacman/src/group23/pacman/controller/MapSelectionViewController.java
+++ JavaPacman/src/group23/pacman/controller/MapSelectionViewController.java
@@ -1,7 +1,11 @@
 package group23.pacman.controller;
 
 import group23.pacman.MainApp;
+import group23.pacman.view.BackgroundAnimationManager;
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ToggleButton;
 import sun.applet.Main;
 
@@ -18,6 +22,10 @@ public class MapSelectionViewController extends RootController implements Joysti
     @FXML private ToggleButton map_2;
     @FXML private ToggleButton map_3;
 
+    BackgroundAnimationManager backgroundAnimationManagerPacman;
+    BackgroundAnimationManager backgroundAnimationManagerGhost;
+    BackgroundAnimationManager backgroundAnimationManagerGhost2;
+
     /**
      * navigation handler
      */
@@ -27,6 +35,44 @@ public class MapSelectionViewController extends RootController implements Joysti
         super("/group23/pacman/view/MapSelectionViewController.fxml");
         adapter.addRow(map_0,map_1,map_2,map_3);
         adapter.current().setSelected(true);
+
+        Canvas canvas = new Canvas(1440, 1000);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        setUpBackgroudAnimations(gc);
+
+        getRoot().getChildren().add(canvas);
+
+        startAnimations();
+    }
+
+    public void startAnimations() {
+        new AnimationTimer() {
+
+            @Override
+            public void handle(long now) {
+                backgroundAnimationManagerPacman.startMoving();
+                backgroundAnimationManagerGhost.startMoving();
+                backgroundAnimationManagerGhost2.startMoving();
+
+            }
+        }.start();
+    }
+
+    public void stopAnimations() {
+        backgroundAnimationManagerPacman.stop();
+        backgroundAnimationManagerGhost.stop();
+        backgroundAnimationManagerGhost2.stop();
+    }
+
+    public void setUpBackgroudAnimations(GraphicsContext gc) {
+        backgroundAnimationManagerPacman = new BackgroundAnimationManager(-150, 900, 1590, "pacman", gc, 'R', 0.3f);
+        backgroundAnimationManagerGhost = new BackgroundAnimationManager(-60, 900, 1590, "ghost1", gc, 'R', 0.3f);
+        backgroundAnimationManagerGhost2 = new BackgroundAnimationManager(0, 900, 1590, "ghost2", gc, 'R', 0.3f);
+
+        getRoot().getChildren().add(backgroundAnimationManagerPacman);
+        getRoot().getChildren().add(backgroundAnimationManagerGhost);
+        getRoot().getChildren().add(backgroundAnimationManagerGhost2);
     }
 
     @Override
